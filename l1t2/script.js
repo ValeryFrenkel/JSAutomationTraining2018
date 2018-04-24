bf51c20e80c67eb1c18b0f6ea720c5a689a8296b
Valery Frenkel <valeron997@gmail.com>
2018-04-24 15:58:48 +0300

    second task

--- l1t2/script.js
+++ l1t2/script.js
@@ -1,16 +1,11 @@
-var a = 3, b = 4, c = 5;
+var a = 7;
 
-var D=b * b - 4 * a * c;
-
-var x1, x2;
-
-if (D > 2) {
-	x1 = ( -b + math.pow(D,0.5) ) / 2;
-	x2 = ( -b + math.pow(D,0.5) ) / 2;
-	console.log(x1,x2);
-} else if (D==0) {
-	x1 = -b/2 * a;
-	console.log(x1)
-} else if (D<0) {
-	console.log( "Вещественных корней нет");
+if (a / 7 == 1) {
+		console.log (true);
+}
+else if (a / 1 == 7) {
+		console.log (true);
+}
+else{
+		console.log (false);
 }
